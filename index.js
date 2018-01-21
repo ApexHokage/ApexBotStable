56507f6bcebc5335a70a08b05502611aacfd44fc
ApexHokage <35670030+ApexHokage@users.noreply.github.com>
2018-01-21 23:30:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const token = "MjUwMDA1MDI3MTUyNzIzOTY5.CxOi3g.T12XrjS8QW6qjTG6qwkIODRUNhI";
+const token = "MzMzMjMyNzQyNTc1NzY3NTU0.DUal2A.PLqEV6V-QuhnOpX2sEOGQklEbV8";
 var prefix = ".";
 var mention = "<@1930903359700619264>";
 var memberCount = client.users.size;
@@ -10,7 +10,7 @@ var servercount = client.guilds.size;
 client.on("ready", () => {
 	var servers = client.guilds.array().map(g => g.name).join(',');
 	console.log("--------------------------------------");
-console.log('[!]Connexion en cours... \n[!]Veuillez Patienté! \n[!]Les évenement sont après ! :)  \n[!]Les préfix actuelle:  ' + prefix + "\n[!]Mentions = " + mention + "\n[!]Nombre de membres: " + memberCount + "\n[!]Nombre de serveurs: " + servercount);
+console.log('[!]Connexion en cours... \n[!]Veuillez Patienté! \n[!]Les évenement sont après ! :)  \n[!]Les préfix actuelle:  ' + prefix + "\n[!]Mentions = " + mention + "\n[!]Nombre de membres: " + memberCount + "\n[!]Nombre de serveurs: " + servercount + "\n[!]Nom du serveur: " + servername);
 });
 
 client.on('message', message => {
